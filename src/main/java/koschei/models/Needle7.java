db606f53452001b8a2e6c557edfc5cea486fb1db
Ivan <vanya.korobov@yandex.ru>
2024-05-02 19:33:31 +0300

    first commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+
+    private final Deth8 deth8;
+
+    public Needle7(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deth8.toString();
     }
 }
